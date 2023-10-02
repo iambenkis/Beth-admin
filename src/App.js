80efa0dacc4a6ff565943560eb7c452ff31b0c9f
benkis <benkisenge03@gmail.com>
2023-10-02 10:57:04 +0200

    Update App js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import AddBoat from './components/AddBoat'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import User from './components/UserList'
+import Books from './components/ReservationList'
+import Boats from './components/BoatList'
+import Login from './components/login'
+import Protected from './components/protected'
+import Header from './components/header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App   bg-gray-100">
+      <Router>
+        <Protected>
+          <>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Header />} />
+              <Route path="/add-boat" element={<AddBoat />} />
+              <Route path="/users" element={<User />} />
+              <Route path="/books" element={<Books />} />
+              <Route path="/boats" element={<Boats />} />
+            </Routes>
+          </>
+        </Protected>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
